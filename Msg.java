4b777b3f04c3c4c4388282676ee14e80005f0796
test <test@example.com>
2018-07-24 01:49:40 +0800

    commit desc

---
+++ Msg.java
@@ -0,0 +1,27 @@
+package com.example.lenovo.uicustomview;
+
+/**
+ * Created by lenovo on 2018/7/15.
+ */
+
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
